9fa1d9f8328492f0fb4a7c87a28f0f4576fa766b
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2019-06-11 17:50:08 +0530

    welcome to the Java

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args){
+        /*system is a class, out is a static propery of that class
+        println is function
+         */
+        System.out.println("Hello World!");
+        System.out.println("Ganesh !");
+    }
+}
