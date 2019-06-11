eefe7fea3c919bad14478015840b2f2a90f4e57f
Ganesh Londhe <ganeshlondhe2020@gmail.com>
2019-06-11 18:00:14 +0530

    class is a blueprint of the real world object

---
+++ practicePrograms/Chapter 02/02_02/begin/src/Car.java
@@ -0,0 +1,17 @@
+//abstract window toolkit library
+import java.awt.*;
+
+//classes are blueprint of the real world object
+public class Car{
+    /*Data types
+    int -> integer 1,2,3
+    double -> decimal 3.4, 4.5, 3.3
+    Color -> awt library
+    boolean -> true or false
+     */
+
+    int averageMPG;
+    String licensePlate;
+    Color paintColor;
+    boolean areTailingWorking;
+}
