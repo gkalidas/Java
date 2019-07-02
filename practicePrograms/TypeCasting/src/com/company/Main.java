eb449cb90a3887260c48364498c83416bd55740b
gkalidas_laptop <ganeshlondhe2020@gmail.com>
2019-07-02 13:54:48 +0530

    typecasting of int to float and if we do reverse the order we'll be loosing the data(decimal points from the float)

---
+++ practicePrograms/TypeCasting/src/com/company/Main.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        int a=9;
+        float b = 10.4F;
+        //a = (float) b;
+        b = (int) a;
+
+        System.out.println("This is typeCasting of int a = "+ a+"  to float b = " + b);
+
+    }
+}
