acfd4f3d0d56fca0c8455b85619c4ca2bc417f05
gkalidas_laptop <ganeshlondhe2020@gmail.com>
2019-07-05 22:56:20 +0530

    Data structure in Java, linked list implementation

---
+++ DataStructure/src/LinkedList.java
@@ -0,0 +1,34 @@
+
+public class LinkedList {
+	Node head;
+	
+	public void insert(int data) {
+		Node node = new Node();
+		node.data = data;
+		//by default object is null
+		//below line is for user ref only
+		node.next = null;
+		
+		if(head == null) {
+			head = node;
+		}
+		else {
+			//
+			Node n = head;
+			while(n.next!=null) {
+				 n = n.next;
+			}
+			n.next = node;
+		}
+	}
+	public void display() {
+		Node node = head;
+		
+		while(node!=null) {
+			System.out.println(node.data);
+			node = node.next;
+		}
+		
+	}
+
+}
---
+++ DataStructure/src/Runner.java
@@ -0,0 +1,14 @@
+
+public class Runner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList list = new LinkedList();
+		list.insert(13);
+		list.insert(45);
+		list.insert(25);
+		
+		list.display();
+	}
+
+}
