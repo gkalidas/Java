3d416e5d98d9408acd2a2b76944ca6f3d8c84de3
gkalidas_laptop <ganeshlondhe2020@gmail.com>
2019-07-09 22:15:34 +0530

    stack implementation using array push, pop, peek

--- DataStructure/src/Runner.java
+++ DataStructure/src/Runner.java
@@ -2,17 +2,28 @@
 public class Runner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		LinkedList list = new LinkedList();
-		list.insert(13);
-		list.insert(45);
-		list.insert(25);
-		list.insertAtStart(56);
+
+		Stack num = new Stack();
 		
-		list.insertAt(2,46);
-		list.display();
-		list.deleteAt(3);
-		list.display();
+		num.push(3);
+		num.push(2);
+		System.out.print(num.peek()+" This is peek\n");
+		num.push(4);
+		
+		System.out.print(num.pop()+" This is pop\n");
+		
+		
+		num.display();
+//		LinkedList list = new LinkedList();
+//		list.insert(13);
+//		list.insert(45);
+//		list.insert(25);
+//		list.insertAtStart(56);
+//		
+//		list.insertAt(2,46);
+//		list.display();
+//		list.deleteAt(3);
+//		list.display();
 	}
 
 }
